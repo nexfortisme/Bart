7daf80dadc525c4d1ddff6eea98b1f7b0c4fa568
David <nexfortisme+github@pm.me>
2026-03-18 10:16:03 -0400

    Removed legacy parts of command interface

--- internal/bot/commands/types.go
+++ internal/bot/commands/types.go
@@ -5,7 +5,4 @@ import "github.com/bwmarrin/discordgo"
 type Command interface {
 	ApplicationCommand() *discordgo.ApplicationCommand
 	Execute(s *discordgo.Session, i *discordgo.InteractionCreate)
-	// HelpString() string
-
-	// CommandCost() int
 }
