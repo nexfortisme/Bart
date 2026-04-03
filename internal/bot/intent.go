3dec2f762710f33bdaccb0149628dd0cd04042a0
David <nexfortisme+github@pm.me>
2026-04-03 11:13:45 -0400

    Added confidence hurdle for intent checking to pass over

--- internal/bot/intent.go
+++ internal/bot/intent.go
@@ -8,17 +8,23 @@ import (
 )
 
 const (
-	PRINT_INTENT_RESULTS = false
+	PRINT_INTENT_RESULTS = true
+	MIN_CONFIDENCE       = 0.7
 )
 
 func MessageIntendedForBartClassifier(message string, stores map[string]*classifier.MemoryStore) classifier.MessageIntent {
 	result, err := classifier.NewClassifier(classifier.NewLMStudioEmbedder(os.Getenv("LLM_BASE_URL"), os.Getenv("EMBEDDING_MODEL")), stores["message_intent"]).WithThreshold(0.7).Classify(message)
 	if err != nil {
+		fmt.Printf("Error classifying message intent: %v\n", err)
 		return classifier.MessageIntentAmbiguous
 	}
 
 	if PRINT_INTENT_RESULTS {
 		printIntentResults(message, result)
+	}
+
+	if result.Confidence < MIN_CONFIDENCE {
+		return classifier.MessageIntentAmbient
 	}
 
 	return classifier.MessageIntent(result.Intent)
@@ -27,11 +33,15 @@ func MessageIntendedForBartClassifier(message string, stores map[string]*classif
 func ToolIntentClassifier(message string, stores map[string]*classifier.MemoryStore) classifier.ToolIntent {
 	result, err := classifier.NewClassifier(classifier.NewLMStudioEmbedder(os.Getenv("LLM_BASE_URL"), os.Getenv("EMBEDDING_MODEL")), stores["tool_intent"]).WithThreshold(0.5).Classify(message)
 	if err != nil {
-		return classifier.ToolIntentNull
+		return classifier.ToolIntentNull // TODO - Update to be ambiguous
 	}
 
 	if PRINT_INTENT_RESULTS {
 		printIntentResults(message, result)
+	}
+
+	if result.Confidence < MIN_CONFIDENCE {
+		return classifier.ToolIntentNull // TODO - Update to be ambiguous
 	}
 
 	return classifier.ToolIntent(result.Intent)
